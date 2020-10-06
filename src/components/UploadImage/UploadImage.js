7b4b7f433ce33e7fb3fe2cceb5cbbdd422a58ed0
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-10-06 16:51:17 -0400

    We did the upload image part of the code, we have a couple of bugs to work out. We were successful in creating the upload image bar though.
    @Ralph-Francesconi007
    @robrichardsdpt
    @nugrezo
    @scottlurowist

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,17 +1,21 @@
 import React from 'react'
-
+// import awsSdk from '../../api/dist/aws-sdk.min'
 class UploadImage extends React.Component {
   constructor ({ user }) {
     super()
     this.state = user
   }
+  onChangeHandler = () => {
+    alert('hi from the handler')
+  }
   render () {
+    // const file = document.getElementById("photoupload").files[0]
     return (
       <div>
-        <h1>This is where to upload an image</h1>
+        <h3>This is where to upload an image</h3>
+        <input id="photoupload" onChange={this.onChangeHandler} type="file" accept="image/*" />
       </div>
     )
   }
 }
-
 export default UploadImage
