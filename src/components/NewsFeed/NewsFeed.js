413f82524ad864869bf3a96b5a3bd741863c5aca
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-10-07 12:34:36 -0400

    Styled the News Feed page and profile page for the way we want it to look.
    Working on the margins and padding for the profile and Newsfeed pages.
    
    @Ralph-Francesconi007
    @robrichardsdpt

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
 class NewsFeed extends React.Component {
   constructor ({ user }) {
@@ -8,7 +9,19 @@ class NewsFeed extends React.Component {
   render () {
     return (
       <div>
-        <h1>This is the NewsFeed</h1>
+        <Card style={{ width: '24rem' }} >
+          <div className="pt-4 pr-2 pl-2 pb-2 mb-0 bg-gradient-primary text-white">
+            <div className="bg-gradient-dark">
+              <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
+              <Card.Body>
+                <Card.Title>Kruger Character</Card.Title>
+                <Card.Text>
+              This is a popular Character from a video game Call of Duty
+                </Card.Text>
+              </Card.Body>
+            </div>
+          </div>
+        </Card>
       </div>
     )
   }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardGroup'
 
 class Profile extends React.Component {
   constructor ({ user }) {
@@ -8,7 +10,54 @@ class Profile extends React.Component {
   render () {
     return (
       <div>
-        <h1>This is the Profile</h1>
+        <h2>This is the Profile</h2>
+        <CardDeck>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
+              </div>
+            </div>
+          </Card>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary text-white">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/cbMvql7.jpg" />
+              </div>
+            </div>
+          </Card>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary text-white">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/PoinHxt.jpg" />
+              </div>
+            </div>
+          </Card>
+        </CardDeck>
+        <div row="4"></div>
+        <CardDeck>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary text-white">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
+              </div>
+            </div>
+          </Card>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary text-white">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
+              </div>
+            </div>
+          </Card>
+          <Card style={{ height: '180px' }} size="4">
+            <div className="bg-gradient-primary text-white">
+              <div className="bg-gradient-dark">
+                <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
+              </div>
+            </div>
+          </Card>
+        </CardDeck>
       </div>
     )
   }
