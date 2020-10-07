722e30aef001c5b650050d857f32b74a9dffde5e
Ergun Oz <ergun.oz82@gmail.com>
2020-10-07 16:14:45 -0400

    Upload Image Component updated. Ready to test with the backend @scottlurowist @nugrezo

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import axios from 'axios'
 // import awsSdk from '../../api/dist/aws-sdk.min'
 class UploadImage extends React.Component {
   constructor ({ user }) {
     super()
     this.state = user
   }
-  onChangeHandler = () => {
-    alert('hi from the handler')
+  handleUploadFile = (event) => {
+    const data = {
+      'userImage':
+      {
+        'file': event.target.files[0],
+        'fileName': event.target.files[0].name,
+        'tag': 'testTag',
+        'description': 'testDescription'
+      }
+    }
+    axios.post('/userImages', data).then((response) => {
+
+    })
   }
   render () {
     // const file = document.getElementById("photoupload").files[0]
     return (
       <div>
         <h3>This is where to upload an image</h3>
-        <input id="photoupload" onChange={this.onChangeHandler} type="file" accept="image/*" />
+        <input id="photoupload" onChange={this.handleUploadFile} type="file" accept="image/*" />
       </div>
     )
   }
