1ad0a82ee1faf52c808f828ff7c598235a78728c
robrichardsdpt <robrichardsdpt@gmail.com>
2020-10-08 15:48:33 -0400

    Create Image fixed to work with backend locally
    Still need to be able to upload to AWS through front end
    @Ralph-Francesconi007
    @robrichardsdpt
    @nugrezo
    @scottlurowist

--- src/components/UploadImage/UploadImage.js
+++ src/components/UploadImage/UploadImage.js
@@ -1,19 +1,85 @@
 import React from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+// import { Redirect } from 'react-router-dom'
 // import awsSdk from '../../api/dist/aws-sdk.min'
 class UploadImage extends React.Component {
-  constructor ({ user }) {
-    super()
-    this.state = user
+  constructor (props) {
+    super(props)
+    console.log(this.props.user)
+    this.state = {
+      userImage: {
+        file: null,
+        fileName: '',
+        description: '',
+        tag: '',
+        owner: ''
+      },
+      createdUserImageId: null,
+      token: this.props.user.token
+    }
   }
-  onChangeHandler = () => {
-    alert('hi from the handler')
+  onDescriptionChangeHandler = (event) => {
+    const userInput = event.target.value
+    const userImageCopy = Object.assign({}, this.state.userImage)
+    userImageCopy.description = userInput
+    this.setState({
+      userImage: userImageCopy
+    })
+  }
+
+  onTagChangeHandler = (event) => {
+    const userInput = event.target.value
+    const userImageCopy = Object.assign({}, this.state.userImage)
+    userImageCopy.tag = userInput
+    this.setState({
+      userImage: userImageCopy
+    })
+  }
+
+  onImageChangeHandler = (event) => {
+    const userInput = event.target.files[0].name
+    const userFile = event.target.files[0]
+    const userImageCopy = Object.assign({}, this.state.userImage)
+    userImageCopy.fileName = userInput
+    userImageCopy.file = userFile
+    this.setState({
+      userImage: userImageCopy
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const userImage = this.state.userImage
+    axios({
+      url: `${apiUrl}/userImages`,
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + `${this.state.token}`
+      },
+      data: {
+        userImage: userImage
+      }
+    })
+      .then((response) => this.setState({
+        createdUserImageId: response.data.userImage._id
+      })
+      )
   }
   render () {
     // const file = document.getElementById("photoupload").files[0]
+    // if (this.state.createdUserImageId !== '') {
+    //   return <Redirect to='/userImage' />
+    // }
     return (
       <div>
-        <h3>This is where to upload an image</h3>
-        <input id="photoupload" onChange={this.onChangeHandler} type="file" accept="image/*" />
+        <h3>Upload an image</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input name="fileName" id="photoupload" onChange={this.onImageChangeHandler} type="file" accept="image/*" />
+          <input name="description" placeholder="Description" type="text" value={this.state.userImage.description} onChange={this.onDescriptionChangeHandler}/>
+          <input name="tag" type="text" placeholder="tag" value={this.state.userImage.tag} onChange={this.onTagChangeHandler}/>
+          <input type="submit" value="submit"/>
+        </form>
       </div>
     )
   }
