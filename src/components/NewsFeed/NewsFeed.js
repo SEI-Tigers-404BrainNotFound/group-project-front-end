cc677e759592eff8753c375a485eefadffc60c89
ralph.francesconi007 <ralph.francesconi007@gmail.com>
2020-10-08 14:14:06 -0400

    I got a link on the email of the user in the news feed so that when you click the email it goes to their profile.

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,18 +1,19 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 
 class NewsFeed extends React.Component {
-  constructor ({ user }) {
-    super()
+  constructor (props, user) {
+    super(props)
     this.state = user
   }
   render () {
+    console.log(this.props.user)
     return (
       <div>
         <Card style={{ width: '24rem' }} >
           <div className="pt-2 pr-2 pl-2 pb-2 mb-0 bg-gradient-primary text-white">
-            <Card.Header>r@r</Card.Header>
+            <Card.Header><Link className="linkIt"to={'/user-profile'}>{this.props.user.email}</Link></Card.Header>
             <div className="bg-gradient-dark">
               <Card.Img variant="top" src="https://i.imgur.com/8whqMYk.jpg" />
               <Card.Body>
