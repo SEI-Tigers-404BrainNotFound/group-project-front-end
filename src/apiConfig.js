f0a26b850463cdd0bed8ee3dc762fd3bdf3b82bf
robrichardsdpt <robrichardsdpt@gmail.com>
2020-10-11 22:13:08 -0400

    updated heroku URL for deployment to https://young-gorge-48445.herokuapp.com/

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://young-gorge-48445.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
