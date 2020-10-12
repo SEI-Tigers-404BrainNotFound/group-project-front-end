06c23a8b663378830a8ca12fb62b3e6ba1bffc6c
robrichardsdpt <robrichardsdpt@gmail.com>
2020-10-11 21:02:51 -0400

    fixed sign in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -15,7 +15,7 @@ const authenticatedOptions = (
 const unauthenticatedOptions = (
   <Fragment>
     <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link href="#/">Sign In</Nav.Link>
   </Fragment>
 )
 
